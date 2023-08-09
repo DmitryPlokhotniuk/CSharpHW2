dbbdd36dc13b54f79687471ee670260d223798c2
DmitPloh <kroll.88@mail.ru>
2023-08-09 22:06:46 +0300

    finished task3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] days = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+Console.WriteLine("Введите номер дня недели и узнайте выходной ли это:");
+int number = Convert.ToInt32(Console.ReadLine()) - 1;
+if (number == 5 | number == 6)
+{
+    Console.WriteLine($"Да, {days[number]} это выходной");
+}
+else
+{
+    Console.WriteLine($"Нет, {days[number]} это не выходной");
+}
