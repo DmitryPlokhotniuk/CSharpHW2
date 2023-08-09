6955707b93d35f75f61fade9b580ee65a9f431f5
DmitPloh <kroll.88@mail.ru>
2023-08-09 21:39:41 +0300

    finished task2

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberShadow = number; //Дублирую переменную для корректной работы строки 17
+if (number / 100 == 0)
+{
+    Console.WriteLine($"Третьей цифры в числе {number} нет");
+}
+else
+{
+    while (numberShadow > 999)
+    {
+        numberShadow = numberShadow / 10;
+    }
+
+    int a = numberShadow % 100;
+    int b = a % 10;
+    Console.WriteLine($"Третья цифра в числе {number} это {b}");
+}
