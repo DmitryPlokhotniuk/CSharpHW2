7134322d3fe28358d23a0bcc7830796f2631a580
DmitPloh <kroll.88@mail.ru>
2023-08-09 21:19:58 +0300

    finished task1

---
+++ task1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int a = number % 100;
+int b = a / 10;
+Console.WriteLine($"Вторая цифра числа это {b}");
